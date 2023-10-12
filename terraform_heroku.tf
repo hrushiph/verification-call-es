f295edcc759236c00762f0691642e0dc134f65f0
hrushiph <83677453+hrushiph@users.noreply.github.com>
2023-10-12 18:19:45 -0500

    Updated WID

--- terraform_heroku.tf
+++ terraform_heroku.tf
@@ -20,7 +20,7 @@ variable "twilio" {
   default = {
     account_sid = "ACe2b8d8a339e6c1285667718cd754b9be"
     auth_token = "ffd0ea7e1b8ea9f93c917257549d0a6f"
-    workspace_sid = "WS4d8e9d4ce0d0c4ba37446d1d399b25e2"
+    workspace_sid = "WS9242394649fc970ea66691b698cbe851"
     chat_service_sid = "IS69bb745859a94bdf94bc955e85475ef6"
     api_key_sid = "SKef01159a993f0c471af82034deb7c9a2"
     api_key_secret = "4e45LE1s8GtUnpcJVl2uc3URIvlE8SUl"
